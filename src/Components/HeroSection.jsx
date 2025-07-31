78c47ffa651d3c4a3ee8658251ddd4d6189662bb
Shukardeen <sonugahlot57016@gmail.com>
2025-07-31 17:52:02 +0530

    updated hero section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -11,23 +11,23 @@ function HeroSection() {
         <source src="https://videos.pexels.com/video-files/2239241/2239241-hd_1920_1080_24fps.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="relative z-10 text-center px-6 sm:px-8 lg:px-10 max-w-6xl mx-auto flex flex-col justify-center items-center mt-8 md:m-0">
+      <div className="relative z-10 text-center px-6 sm:px-8 lg:px-10 max-w-6xl mx-auto flex flex-col justify-center items-center mt-2 md:m-0">
         <h1 className="text-5xl md:text-7xl lg:text-6xl font-bold tracking-tight leading-tight whitespace-nowrap">
           Capture Master
         </h1>
         <p className="text-xl md:text-2xl lg:text-3xl mb-10 text-gray-300 font-light">
           Photography & Films
         </p>
-        <div className="flex flex-col sm:flex-row gap-6 justify-center items-center">
+        <div className="flex justify-center items-center gap-6">
           <button
             onClick={() => scrollToSection("portfolio")}
-            className="bg-primary hover:bg-white text-secondary px-10 py-4 rounded-xl font-semibold transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl cursor-pointer"
+            className="h-14 w-fit whitespace-nowrap bg-primary hover:bg-white text-secondary px-4 py-4 rounded-xl font-semibold transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl cursor-pointer"
           >
             View Our Work
           </button>
           <button
             onClick={() => scrollToSection("contact")}
-            className="border-2 border-white text-white hover:bg-white hover:text-gray-900 px-10 py-4 rounded-xl font-semibold transition-all duration-300 transform hover:scale-105 cursor-pointer"
+            className="h-14 w-fit whitespace-nowrap border-2 border-white text-white hover:bg-white hover:text-gray-900 px-4 py-4 rounded-xl font-semibold transition-all duration-300 transform hover:scale-105 cursor-pointer"
           >
             Get In Touch
           </button>
@@ -35,7 +35,7 @@ function HeroSection() {
       </div>
 
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 animate-bounce">
         <svg
           className="w-8 h-8 text-primary"
           fill="none"
